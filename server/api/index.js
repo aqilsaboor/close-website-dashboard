fe107956b8107a4b7729c0e5a62c6087a046dd93
githubSubhanKhan <subbikhan124@gmail.com>
2025-11-25 21:01:26 +0500

    created an api route

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+// require('dotenv').config({ path: '../.env' });
 
 const app = express();
 const PORT = 5000;
 
+const dataRoutes = require('./routes/data');
+
 // Middleware to parse JSON
 app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
+app.use('/api/data', dataRoutes);
 // Basic route
 app.get("/", (req, res) => {
   res.send("✅ Backend server is running!");
@@ -15,3 +23,5 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
+
+module.exports = app;
