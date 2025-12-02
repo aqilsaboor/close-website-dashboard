13b8d9e4f791fa2844c66ac94b0de49e9c3c3bd4
Aqil Saboor <naveedsaboor0@gmail.com>
2025-12-02 23:35:09 +0500

    revert index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,23 +1,29 @@
 const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-require("dotenv").config();
-import serverless from "serverless-http";
+const cors = require('cors');
+const bodyParser = require('body-parser');
+require('dotenv').config();
 
 const app = express();
-const closeRoutes = require("./routes/closeRoutes");
+const PORT = 5000;
 
-// Middleware
+
+const closeRoutes = require('./routes/closeRoutes');
+
+// Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-// Routes
-app.use("/api/close", closeRoutes);
+app.use('/api/close', closeRoutes);
 
+// Basic route
 app.get("/", (req, res) => {
-  res.send("Backend server is running!");
+  res.send("✅ Backend server is running!");
 });
 
+// Start server
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
 
-export default serverless(app);
+module.exports = app;
