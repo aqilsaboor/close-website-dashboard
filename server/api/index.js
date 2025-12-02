96afc5ce9c57b2ba09f89b67155fb2f7c8624a70
Aqil Saboor <naveedsaboor0@gmail.com>
2025-12-02 22:22:55 +0500

    changes for vercel by ahmed

--- server/api/index.js
+++ server/api/index.js
@@ -1,29 +1,23 @@
 const express = require("express");
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+import serverless from "serverless-http";
 
 const app = express();
-const PORT = 5000;
+const closeRoutes = require("./routes/closeRoutes");
 
-
-const closeRoutes = require('./routes/closeRoutes');
-
-// Middleware to parse JSON
+// Middleware
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use('/api/close', closeRoutes);
+// Routes
+app.use("/api/close", closeRoutes);
 
-// Basic route
 app.get("/", (req, res) => {
-  res.send("✅ Backend server is running!");
+  res.send("Backend server is running!");
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
-});
 
-module.exports = app;
+export default serverless(app);
